b7eed93831c17d9cf2c8418f91a520ad73df6198
mateus <mateusangeljr@mail.com>
2023-06-24 23:22:18 -0300

    deploy

--- src/Templates/main.jsx
+++ src/Templates/main.jsx
@@ -1,20 +1,18 @@
-/* eslint-disable no-undef */
-// import React from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "../Templates/App/App";
 import { ThemeProvider } from "styled-components";
 import { Themes } from "../styles/Theme";
 import { GlobalStyled } from "../styles/GlobalSyled";
 import { ContextProvider } from "../Context/ContextProvider/ContextProvider";
+import React from "react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  // <React.StrictMode>
-
-  <ContextProvider>
-    <ThemeProvider theme={Themes}>
-      <App />
-      <GlobalStyled />
-    </ThemeProvider>
-  </ContextProvider>
-  // </React.StrictMode>
+  <React.StrictMode>
+    <ContextProvider>
+      <ThemeProvider theme={Themes}>
+        <App />
+        <GlobalStyled />
+      </ThemeProvider>
+    </ContextProvider>
+  </React.StrictMode>
 );
